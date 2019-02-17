21f14b4f2c41e59dd4eaf7ac20880694f31617d3
Tapaka <maiga.maiga@hotmail.fr>
2019-02-17 23:21:21 +0100

    Refacto, prise en compte "Serie S01E01.format"

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,26 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Main {
-    static Map<String, Integer[]> mapFileSeason = new HashMap<>();
+    private static Map<String, Integer[]> mapFileSeason;
+    private static Boolean foundSplitRequirement, episodeRegex;
+    private static String seasonEpisode;
+    private static Integer seasonSplit, season, episode;
+    private final static String[] regexList;
+
+    static{
+        mapFileSeason = new HashMap<>();
+        regexList = new String[]{"(S[0-9]{2}E[0-9]{2})", "(Episode|episode)", "(S[0-9]{2}\\.E[0-9]{2})"};
+        initializeGlobalValues();
+    }
+
+    private static void initializeGlobalValues(){
+        foundSplitRequirement = false;
+        episodeRegex = false;
+        seasonEpisode = "";
+        seasonSplit = 0;
+        season = 1;
+        episode = 0;
+    }
     public static void main(String[] args) {
         File folder = new File("F:\\TVShows");
         File[] listOfFiles = folder.listFiles();
@@ -16,12 +35,7 @@ public class Main {
             goDeeper(listOfFiles[i]);
         }
 
-        Stream<Entry<String, Integer[]>> mapSorted = mapFileSeason.entrySet().stream().sorted(Entry.comparingByKey());
-        mapSorted.forEach(x -> System.out.println(x.getKey() + " : S"+addZeros(x.getValue()[0])+"E"+addZeros(x.getValue()[1])));
-
-//        for(Entry<String, Integer[]> entry : mapFileSeason.entrySet()){
-////            System.out.println(entry.getKey() + " : S"+addZeros(entry.getValue()[0])+"E"+addZeros(entry.getValue()[1]));
-////        }
+        displayTvShows();
     }
 
     private static void goDeeper(File file){
@@ -34,8 +48,12 @@ public class Main {
         }
     }
 
-    private static Boolean episodeRegex = false;
-    private final static String[] regexList = new String[]{"(S[0-9]{2}E[0-9]{2})", "(Episode|episode)", "(S[0-9]{2}\\.E[0-9]{2})"};
+    private static void displayTvShows(){
+        Stream<Entry<String, Integer[]>> mapSorted = mapFileSeason.entrySet().stream().sorted(Entry.comparingByKey());
+        mapSorted.forEach(x -> System.out.println(x.getKey() + " : S"+addZeros(x.getValue()[0])+"E"+addZeros(x.getValue()[1])));
+    }
+
+
 
     private static void getSplitRegex(String splitString, Integer currentIndex){
         if(episodeRegex){
@@ -58,23 +76,20 @@ public class Main {
 
     }
 
-    private static Boolean foundSplitRequirement = false;
-    private static String seasonEpisode = "";
-    private static Integer seasonSplit = 0;
-
-    private static void resetGlobalValues(){
-        foundSplitRequirement = false;
-        episodeRegex = false;
-        seasonEpisode = "";
-        seasonSplit = 0;
-    }
-
     private static void processFile(File file){
-        resetGlobalValues();
-        String[] splitString = file.getName().split("\\.").length < 3 ? file.getName().split("\\s") : file.getName().split("\\.");
-        if(file.getName().contains("code")){
-            System.out.println("");
+        initializeGlobalValues();
+        String[] splitOnDot = file.getName().split("\\.");
+        String[] splitOnSpace = file.getName().split("\\s");
+        String[] splitString = null;
+        if(splitOnDot.length < splitOnSpace.length){
+            splitString = splitOnSpace;
+        }else if(splitOnDot.length > splitOnSpace.length){
+            splitString = splitOnDot;
+        }else if(splitOnDot.length == splitOnSpace.length){
+            //Cas "Serie S01E01.mp4"
+            splitString = file.getName().substring(0, file.getName().indexOf('.')).split("\\s");
         }
+
         for(int k = 0; k < splitString.length; k++){
             if(!foundSplitRequirement) {
                 getSplitRegex(splitString[k], k);
@@ -82,6 +97,41 @@ public class Main {
                 break;
             }
         }
+
+        String showName = getShowName(splitString);
+        getSeasonEpisode();
+
+        setMap(episode, season, showName);
+    }
+
+    private static void getSeasonEpisode(){
+        Boolean setSeason = false;
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(seasonEpisode);
+        while(m.find()) {
+            if (!setSeason && season == 1 && (seasonEpisode.matches(regexList[0]) || seasonEpisode.matches(regexList[2]))) {
+                setSeason = true;
+                season = Integer.valueOf(m.group());
+            } else {
+                episode = Integer.valueOf(m.group());
+            }
+        }
+    }
+
+    private static void setMap(Integer episode, Integer season, String showName){
+        if(episode != 0){
+            if(!mapFileSeason.containsKey(showName)){
+                mapFileSeason.put(showName.toLowerCase(), new Integer[]{season, episode});
+            }else {
+                Integer[] latestSeasonEpisode = mapFileSeason.get(showName);
+                if (season >= latestSeasonEpisode[0] && episode > latestSeasonEpisode[1]) {
+                    mapFileSeason.put(showName, new Integer[]{season, episode});
+                }
+            }
+        }
+    }
+
+    private static String getShowName(String[] splitString){
         StringBuilder sb = new StringBuilder();
         for(int l = 0; l < seasonSplit; l++){
             if(!splitString[l].matches("([0-9]{4})")) {
@@ -91,31 +141,7 @@ public class Main {
                 }
             }
         }
-        String showName = sb.toString().toLowerCase();
-        if(showName.equals("code geass")){
-            System.out.printf("");
-        };
-        Pattern p = Pattern.compile("\\d+");
-        Matcher m = p.matcher(seasonEpisode);
-        Integer season = 1, episode = 0;
-        while(m.find()) {
-            if (season == 0 && (seasonEpisode.matches(regexList[0]) || seasonEpisode.matches(regexList[2]))) {
-                season = Integer.valueOf(m.group());
-            } else {
-                episode = Integer.valueOf(m.group());
-            }
-        }
-
-        if(episode != 0){
-            if(!mapFileSeason.containsKey(showName)){
-                mapFileSeason.put(sb.toString().toLowerCase(), new Integer[]{season, episode});
-            }else {
-                Integer[] latestSeasonEpisode = mapFileSeason.get(showName);
-                if (season >= latestSeasonEpisode[0] && episode > latestSeasonEpisode[1]) {
-                    mapFileSeason.put(showName, new Integer[]{season, episode});
-                }
-            }
-        }
+        return sb.toString();
     }
 
     private static String addZeros(Integer i){
